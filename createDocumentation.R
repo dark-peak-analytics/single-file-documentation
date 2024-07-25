0e18c131db5f5913e542c311cba56044a27b05ea
bitowaqr <bitowaqr@gmail.com>
2024-07-25 16:19:02 +0200

    init commit

---
+++ createDocumentation.R
@@ -0,0 +1,7 @@
+# install.packages("document")
+document::document(
+  file_name = "./sum.R",
+  output_directory = "./",
+  check_package = F, 
+  check_as_cran = F
+  )
---
+++ sum.R
@@ -0,0 +1,15 @@
+#' sum 2 numbers
+#'  
+#' This function takes 2 numbers and returns their sum
+#' 
+#' @param x a number
+#' @param y a number
+#' @return a number
+#' @examples
+#' sum(1, 2)
+#' sum(3, 4)
+
+sum <- function(x, y) {
+  x + y
+}
+
